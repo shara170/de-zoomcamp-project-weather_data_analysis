a45014f1047c831fbc08c8fd861d0493a92b9b00
Ayushi Sharma <ayushi.2210sharma@gmail.com>
2024-04-09 19:55:04 +0000

    first project commit

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.14.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+# Code to create bucket - START
+resource "google_storage_bucket" "weather-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+# Code to create bucket - END
+
+# Code to create a BigQuery dataset - START
+
+resource "google_bigquery_dataset" "weather_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,37 @@
+variable "credentials" {
+  description = "My credentials"
+  default     = "./keys/my-cred.json"
+
+}
+
+variable "project" {
+  description = "Project"
+  default     = "silicon-mile-412319"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery dataset name"
+  default     = "weather_data"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "silicon-mile-412319-weather-in"
+
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+
+}
