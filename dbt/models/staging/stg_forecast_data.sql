c1b8db7df89881f7fe60111fe2e0d23905c2032f
Ayushi Sharma <ayushi.2210sharma@gmail.com>
2024-04-11 05:52:42 +0000

    fix stg_forecast_data, removed limit

--- dbt/models/staging/stg_forecast_data.sql
+++ dbt/models/staging/stg_forecast_data.sql
@@ -72,10 +72,3 @@ select
     
 from weather_data
 where rn = 1
-
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
